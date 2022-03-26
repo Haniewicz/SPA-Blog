144db79b89cde59252aa13a04b81fe069b75d1ba
Kamil <haniewicz@interia.pl>
2022-03-26 08:32:35 +0100

    Frontend changes
    Added frontend for Admin panel template and changed linking of external css files (index.html is now more lightweight). Need to figure out how to use optional chaining without destroying syntax highlighting

--- frontend/src/components/admin/Index.vue
+++ frontend/src/components/admin/Index.vue
@@ -2,64 +2,80 @@
 
 <template>
 
-<div>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div class="container">
-            <router-link class="navbar-brand" data-toggle="collapse" :to="{ name: 'home' }">
-                <b>HANIEWKA</b> <span style="color: #B41F1F">BLOG</span>
-            </router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
 
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <ul class="navbar-nav w-100 mr-auto">
-                    <li class="nav-item">
-                        <router-link class="nav-link" data-toggle="collapse" :to="{ name: 'home' }">
-                            Strona główna
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" data-toggle="collapse" :to="{ name: 'home' }">
-                            Kontakt
-                        </router-link>
-                    </li>
-                    <li v-if="$store.getters.token" style="text-align: right;" class="nav-item dropdown ml-auto align-right">
-                        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{$store.getters.user?.name}}</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <router-link class="dropdown-item" data-toggle="collapse" :to="{ name: 'profile' }">
-                                Profil
-                            </router-link>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" @click="logout">Wyloguj</a>
-                        </div>
-                    </li>
-                    <li v-else class="nav-item ml-auto">
-                        <router-link class="nav-link log-in" data-toggle="collapse" :to="{ name: 'login' }">
-                            <b>Zaloguj się</b>
-                        </router-link>
-                    </li>
+
+    <div id="viewport">
+              <!-- Sidebar -->
+              <div id="sidebar">
+                <header>
+                  <a href="#">Panel administratora</a>
+                </header>
+                <ul class="nav">
+                  <li>
+                    <a href="/add_post">
+                      <i class="zmdi zmdi-link"></i> Dodaj wpis
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/posts">
+                      <i class="zmdi zmdi-link"></i> Lista wpisów
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/users">
+                      <i class="zmdi zmdi-link"></i> Lista użytkowników
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/roles">
+                      <i class="zmdi zmdi-link"></i> Zarządzaj rolami
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="zmdi zmdi-info-outline"></i> About
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="zmdi zmdi-settings"></i> Services
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="zmdi zmdi-comment-more"></i> Contact
+                    </a>
+                  </li>
                 </ul>
+              </div>
+              <!-- Content -->
+              <div id="content">
+                <nav class="navbar navbar-default">
+                  <div class="container-fluid">
+                    <ul class="nav navbar-nav navbar-right">
+                      <li>
+                        <a href="#"><i class="zmdi zmdi-notifications text-danger"></i>
+                        </a>
+                      </li>
+                      <li><a href="#" style="color:green">{{$store.getters.user?.name}}</a></li>
+                      <li><a href="/logout" style="color:red">Wyloguj</a></li>
+                    </ul>
+                  </div>
+                </nav>
+                <div class="container-fluid">
+                    <router-view></router-view>
+                </div>
+              </div>
             </div>
-        </div>
-    </nav>
-    <h1>Jeśli to czytasz, to nie wiem jak się tu dostałeś, ale ta strona będzie zmieniona</h1>
-    <router-view></router-view>
-    <div class="container">
-        <div style="clear: both;"></div>
-        <hr style="border-color: #B41F1F;">
-    </div>
-    <footer>
-        <p>HaniewkaBlog ADMIN 2022 - Wszelkie prawa zastrzeżone</p>
-    </footer>
-
-</div>
 
 </template>
 
+
 <script>
 export default {
-
     methods: {
         logout: function(){
             this.$axios.get('sanctum/csrf-cookie').then(response => {
